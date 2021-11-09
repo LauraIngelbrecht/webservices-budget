647d866cb1175fb868e06fb7ef64a7798537e3f1
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-11-09 08:51:32 +0100

    Cleanup tests

--- __tests__/rest/places.spec.js
+++ __tests__/rest/places.spec.js
@@ -1,43 +1,52 @@
 const supertest = require('supertest');
 const createServer = require('../../src/createServer');
-const {getKnex, tables} = require('../../src/data');
+const { getKnex, tables } = require('../../src/data');
 
+const data = {
+  places: [{
+      id: '7f28c5f9-d711-4cd6-ac15-d13d71abff83',
+      name: 'Loon',
+      rating: 5
+    },
+    {
+      id: '7f28c5f9-d711-4cd6-ac15-d13d71abff84',
+      name: 'Bezine',
+      rating: 2
+    },
+    {
+      id: '7f28c5f9-d711-4cd6-ac15-d13d71abff85',
+      name: 'Irish pub',
+      rating: 4
+    }
+  ]
+};
 
-const data={
-    places:[ 
-    { id: '7f28c5f9-d711-4cd6-ac15-d13d71abff83', name: 'Loon', rating: 5 },
-    { id: '7f28c5f9-d711-4cd6-ac15-d13d71abff84', name: 'Bezine', rating: 2 },
-    { id: '7f28c5f9-d711-4cd6-ac15-d13d71abff85', name: 'Irish pub', rating: 4 }]
-  }
-
-const dataToDelete={  
-    places:[
-        '7f28c5f9-d711-4cd6-ac15-d13d71abff83',
-        '7f28c5f9-d711-4cd6-ac15-d13d71abff84',
-        '7f28c5f9-d711-4cd6-ac15-d13d71abff85',
-      ]
-}
+const dataToDelete = {
+  places: [
+    '7f28c5f9-d711-4cd6-ac15-d13d71abff83',
+    '7f28c5f9-d711-4cd6-ac15-d13d71abff84',
+    '7f28c5f9-d711-4cd6-ac15-d13d71abff85',
+  ]
+};
 
 describe('Places', () => {
-    let server;
-    let request;
-    let knex;
+  let server;
+  let request;
+  let knex;
 
-    beforeAll(async () => {
-     jest.setTimeout(1000);
-     server = await createServer();
-     request = supertest(server.getApp().callback());
-     knex = getKnex();
-    });
+  beforeAll(async () => {
+    server = await createServer();
+    request = supertest(server.getApp().callback());
+    knex = getKnex();
+  });
 
-     afterAll(async () => {
-        // Cleanup resources!
-        await server.stop();
-      });
+  afterAll(async () => {
+    await server.stop();
+  });
+
+  const url = '/api/places';
 
   describe('GET /api/places', () => {
-
-    const url = '/api/places';
 
     beforeAll(async () => {
       await knex(tables.place).insert(data.places);
@@ -52,15 +61,15 @@ describe('Places', () => {
     test('it should 200 and return all places', async () => {
       const response = await request.get(url)
 
-      expect(response.statusCode).toBe(200);
-      expect(response.body.count).toBeGreaterThanOrEqual(3); // one place from transactions could be present
-      expect(response.body.data.length).toBeGreaterThanOrEqual(3); // one place from transactions could be present
+      expect(response.status).toBe(200);
+      // one place from transactions could be present due to Jest running all tests parallel
+      // so check at least 3 places exist
+      expect(response.body.count).toBeGreaterThanOrEqual(3);
+      expect(response.body.data.length).toBeGreaterThanOrEqual(3);
     });
-});
+  });
 
-   describe('GET /api/places/:id', () => {
-
-    const url = '/api/places';
+  describe('GET /api/places/:id', () => {
 
     beforeAll(async () => {
       await knex(tables.place).insert(data.places[0]);
@@ -68,21 +77,20 @@ describe('Places', () => {
 
     afterAll(async () => {
       await knex(tables.place)
-        .where('id', [data.places[0].id])
+        .where('id', data.places[0].id)
         .delete();
     });
 
     test('it should 200 and return the requested place', async () => {
-      const response = await request.get(`${url}/7f28c5f9-d711-4cd6-ac15-d13d71abff83`)
-      expect(response.statusCode).toBe(200);
+      const response = await request.get(`${url}/${data.places[0].id}`)
+      expect(response.status).toBe(200);
       expect(response.body).toEqual(data.places[0]);
-    }); 
+    });
   });
 
   describe('POST /api/places', () => {
 
     const placesToDelete = [];
-    const url = '/api/places';
 
     afterAll(async () => {
       await knex(tables.place)
@@ -96,7 +104,7 @@ describe('Places', () => {
           name: 'New place',
         });
 
-      expect(response.statusCode).toBe(201);
+      expect(response.status).toBe(201);
       expect(response.body.id).toBeTruthy();
       expect(response.body.name).toBe('New place');
       expect(response.body.rating).toBeNull();
@@ -111,7 +119,7 @@ describe('Places', () => {
           rating: 5,
         });
 
-      expect(response.statusCode).toBe(201);
+      expect(response.status).toBe(201);
       expect(response.body.id).toBeTruthy();
       expect(response.body.name).toBe('Lovely place');
       expect(response.body.rating).toBe(5);
@@ -119,24 +127,16 @@ describe('Places', () => {
       placesToDelete.push(response.body.id);
     });
   });
- 
+
   describe('PUT /api/places/:id', () => {
 
-    const url = '/api/places';
-
     beforeAll(async () => {
-      await knex(tables.place).insert([
-        { id: '7f28c5f9-d711-4cd6-ac15-d13d71abff83', name: 'Loon', rating: 4 },
-        { id: '7f28c5f9-d711-4cd6-ac15-d13d71abff84', name: 'Duplicate name', rating: 1 },
-      ]);
+      await knex(tables.place).insert(data.places);
     });
 
     afterAll(async () => {
       await knex(tables.place)
-        .whereIn('id', [
-          '7f28c5f9-d711-4cd6-ac15-d13d71abff83',
-          '7f28c5f9-d711-4cd6-ac15-d13d71abff84',
-        ])
+        .whereIn('id', dataToDelete.places)
         .delete();
     });
 
@@ -147,17 +147,16 @@ describe('Places', () => {
           rating: 1,
         });
 
-      expect(response.statusCode).toBe(200);
+      expect(response.status).toBe(200);
       expect(response.body).toEqual({
         id: '7f28c5f9-d711-4cd6-ac15-d13d71abff83',
         name: 'Changed name',
         rating: 1,
       });
-    });   
+    });
   });
 
   describe('DELETE /api/places/:id', () => {
-    const url = '/api/places';
 
     beforeAll(async () => {
       await knex(tables.place).insert(data.places[0]);
@@ -166,7 +165,7 @@ describe('Places', () => {
     test('it should 204 and return nothing', async () => {
       const response = await request.delete(`${url}/${data.places[0].id}`)
 
-      expect(response.statusCode).toBe(204);
+      expect(response.status).toBe(204);
       expect(response.body).toEqual({});
     });
   });
