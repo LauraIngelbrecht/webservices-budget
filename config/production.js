dec286009415f59c3ebe5d6e52dbb47716b57db3
LauraIngelbrecht <79205146+LauraIngelbrecht@users.noreply.github.com>
2021-12-23 14:07:39 +0100

    Update production.js

--- config/production.js
+++ config/production.js
@@ -1,20 +1,38 @@
 module.exports = {
-	log: {
-		level: 'info',
-		disabled: false,
-	},
-	cors: {
-		origins: ['http://localhost:3000'],
-		maxAge: 3 * 60 * 60, // 3h in seconds
-	},
-	database: {
-    client: 'mysql2',
-		host: 'localhost',
-    port: 3306,
-    name: 'budget',
+  log: {
+    level: 'info',
+    disabled: false,
   },
-	pagination: {
+  cors: {
+    origins: ['http://localhost:3000',
+    'https://LauraIngelbrecht.github.io/frontendweb-karine-2122-LauraIngelbrecht'
+  ],
+    maxAge: 3 * 60 * 60, // 3h in seconds
+  },
+  database: {
+    client: 'mysql2',
+    host: 'localhost',
+    port: 3306,
+    name: 'bouwsoft',
+    username:'root',
+    password:'Bouwsoft22',
+  },
+  pagination: {
     limit: 100,
     offset: 0,
+  },
+  auth: {
+    argon: {
+      saltLength: 16,
+      hashLength: 32,
+      timeCost: 6,
+      memoryCost: 2 ** 17,
+    },
+    jwt: {
+      // secret comes via env
+      expirationInterval: 3 * 24 * 60 * 60 * 1000, // ms (3 days)
+      issuer: 'budget.hogent.be',
+      audience: 'budget.hogent.be',
+    },
   },
 };
